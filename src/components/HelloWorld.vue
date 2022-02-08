3aff6ce1c7df9dbce4a3fd7d4edddd8a0c0de5d7
Sergei Aleksandrov <sergei.a.aleks@gmail.com>
2022-02-08 07:46:35 +0500

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    <h5>{{ msg }}</h5>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+h5 {
+  color: #252525;
+}
+</style>
